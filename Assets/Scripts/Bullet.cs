e69b815e999b7b55ce71afb2696640c0d55d3533
KBear100 <kgamett@student.neumont.edu>
2023-01-11 19:52:30 -0700

    Basic Ship

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 60;
+    void Start()
+    {
+        Destroy(gameObject, 5);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
